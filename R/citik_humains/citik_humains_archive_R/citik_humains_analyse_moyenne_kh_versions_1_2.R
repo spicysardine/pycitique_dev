8d651ee2d64714a823488ec6898ad9ad0abda2c7
beetroot <spicy.sardine@yandex.com>
2021-01-28 00:30:05 +0100

    Génération de rapport Rmarkdown

--- R/citik_humains/citik_humains_archive_R/citik_humains_analyse_moyenne_kh_versions_1_2.R
+++ R/citik_humains/citik_humains_archive_R/citik_humains_analyse_moyenne_kh_versions_1_2.R
@@ -95,6 +95,7 @@ weatherStats <- function(paramDSK, paramMF, paramName, SIunit){
 ####### boucle de calcul itératif
 
 ### Définition des listes des paramètres
+
 templist <- list(dskParam=dskdatavg$temperature, mfParam=mfdatavg$temperature, Breaks=1, paramName='Température', SIunit='C°')
 humlist <- list(dskParam=dskdatavg$humidity, mfParam=trunc(mfdatavg$humidite), Breaks=1, paramName='Humidité', SIunit='%')
 ptrlist <- list(dskParam=dskdatavg$dewpoint, mfParam=mfdatavg$point_rose, Breaks=1, paramName='Point de rosée', SIunit='C°')
@@ -112,10 +113,143 @@ length(paramlist)
 for( param in paramlist) {
         
         print(param$paramName)
-        
         ### Histogramme du paramètres moyennes
         weatherHistogram(param$dskParam, param$mfParam, param$Breaks, param$paramName, param$SIunit)
         
         ### Test des distributions statistiques
         weatherStats(param$dskParam, param$mfParam, param$paramName, param$SIunit)
 }
+
+# ############################## Analyse des Températures moyennes ###############################################
+# 
+# ### Affectation des variables:
+# ## initialisation de la donnée.
+# dsktemperature <- dskdatavg$temperature
+# mftemperature <- mfdatavg$temperature
+# 
+# ### Histogramme des Températures moyennes
+# weatherHistogram(dsktemperature, mftemperature, Breaks=1, 'Température', 'C°')
+# 
+# ######### Test des distributions statistiques
+# weatherStats(dsktemperature, mftemperature, 'Température C°')
+# 
+# ############################## Analyse de l'humidité moyenne DSK vs MF #########################################
+# 
+# ### Affectation des variables:
+# ## Attention la donnée de l'humidité MF contient des fractions. e.g: 83.66
+# ## tandis que la donnée DSK n'en contient pas. e.g: 83
+# dskhumidity <- dskdatavg$humidity
+# mfhumidity <- trunc(mfdatavg$humidite)
+# 
+# ### Histogramme de l'humidité moyenne "humidite" pour MF et "humidity" pour DSK
+# weatherHistogram(dskhumidity, mfhumidity, Breaks = 1,'Humidité', '%')
+# 
+# ######### Test de distributions statistiques:
+# weatherStats(dskhumidity, mfhumidity, 'Humidité %')
+# 
+# ############################## Analyse du point de rosé moyen ##########################################
+# 
+# ### Affectation des variables:
+# dskpr <- dskdatavg$dewpoint
+# mfpr <- mfdatavg$point_rose
+# 
+# ############# Histogramme du point de rosé moyen
+# weatherHistogram(dskpr, mfpr, Breaks = 1, 'Point de rosée', 'C°')
+# 
+# ########## Test de distributions statistiques:
+# weatherStats(dskpr, mfpr, 'Point de rosée C°')
+# 
+# 
+# ##############################  Analyse de la pression moyenne au niveau de la mer ##############################
+# 
+# ### Affectation des variables:
+# dskpress <- dskdatavg$pressure
+# mfpress <- mfdatavg$press_mer
+# 
+# ############ Histogramme de la pression moyenne au niveau de la mer "press_mer" pour MF et "pressure" pour DSK
+# weatherHistogram(dskpress, mfpress, Breaks=1 ,'Pression', 'hPa')
+# 
+# ########## Test de distributions statistiques:
+# weatherStats(dskpress, mfpress, 'Pression hPa')
+# 
+# ##############################  Analyse de la vitesse moyenne du vent ###########################################
+# 
+# ### Affectation des variables:
+# dskwindspeed <- dskdatavg$windspeed
+# mfwindspeed <- mfdatavg$vvent
+# 
+# ### Histogramme de la vitesse moyenne du vent "vvent" pour MF et "windspeed" pour DSK
+# weatherHistogram(dskwindspeed, mfwindspeed, Breaks=0.5, 'Vitesse du vent', 'm/s')
+# 
+# ########## Test de distributions statistiques:
+# weatherStats(dskwindspeed, mfwindspeed, 'Vitesse moyenne')
+# 
+# ##############################  Analyse de la visibilite moyenne ################################################
+# 
+# ### Affectation des variables:
+# dskvisibility <- dskdatavg$visibility
+# mfvisibility <- mfdatavg$visibilite
+# 
+# ################ Histogramme de la visibilite moyenne "visibilite" pour MF et "visibility"
+# weatherHistogram(dskvisibility, mfvisibility, Breaks = 1 ,'Visibilité', '%')
+# 
+# ########## Test de distributions statistiques:
+# weatherStats(dskvisibility, mfvisibility, 'Visibilité %')
+# 
+# ##############################  Analyse de la nebulosité moyenne ################################################
+# 
+# ### Affectation des variables:
+# ## Attention la donnée de la nébulosité moyenne MF contient des fractions. e.g: 83.66
+# ## tandis que la donnée DSK n'en contient pas. e.g: 83
+# dskcloud <- dskdatavg$cloudcover
+# mfcloud <- trunc(mfdatavg$nebulosite)
+# 
+# ### Histogramme de la nebulosité moyenne "nebulosite" pour MF et "cloudcover" pour DSK
+# weatherHistogram(dskcloud, mfcloud, Breaks = 2, 'Nébulosité', '%')
+# 
+# ########## Test de distributions statistiques:
+# weatherStats(dskcloud, mfcloud, 'Nébulosité %')
+# 
+# ##############################  Analyse de la rafale_10min moyenne #############################################
+# 
+# ### Affectation des variables:
+# dskwg <- dskdatavg$windgust
+# mfwg <- mfdatavg$rafale_10min
+# 
+# ### Histogramme de la rafale_10min moyenne "rafale_10min" pour MF et "windgust" pour DSK
+# weatherHistogram(dskwg, mfwg, Breaks = .5,'rafale de vent sur 10 min', 'm/s')
+# 
+# ########## Test de distributions statistiques:
+# weatherStats(dskwg, mfwg, 'Rafale de vent sur 10 min m/s')
+# 
+# ##############################  Analyse de la quantité moyenne de précipitation par heure ######################
+# 
+# ### Affectation des variables:
+# dskprecip <- dskdatavg$precipintensity
+# mfprecip <- mfdatavg$precip_01h
+# 
+# ### Histogramme de la precip_24h moyenne "precip_24h" pour MF et "precipintensity" pour DSK
+# weatherHistogram(dskprecip, mfprecip, Breaks=0.1, 'intensité de Précipitation', 'mm/h')
+# 
+# ########## Test de distributions statistiques:
+# weatherStats(dskprecip, mfprecip, 'Précipitation mm/h')
+# 
+# 
+# ##############################  Analyse de la quantité moyenne de précipitation par 24 heure ######################
+# 
+# ### Affectation des variables:
+# dskprecip24 <- dskdatavg$precipintensitymax
+# mfprecip24 <- mfdatavg$precip_24h
+# 
+# ### Histogramme de la precip_24h moyenne "precip_24h" pour MF et "precipintensity" pour DSK
+# weatherHistogram(dskprecip24, mfprecip24, Breaks=0.1, 'intensité de Précipitation', 'mm/h')
+# 
+# ########## Test de distributions statistiques:
+# weatherStats(dskprecip24, mfprecip24, 'Précipitation mm/h')
+# 
+# 
+# 
+# 
+
+
+
