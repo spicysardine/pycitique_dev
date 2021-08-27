15f6f634bd5268ec57c8c4ea2ab2195b818fef0b
beetroot <spicy.sardine@yandex.com>
2021-08-28 00:57:00 +0200

    Indentation et commentaire du code

--- R/citik_humains/smartick_hum_meteo_analyse_statisitique.R
+++ R/citik_humains/smartick_hum_meteo_analyse_statisitique.R
@@ -1,27 +1,30 @@
 
-###############################################################################################################################
-###### Code pour Tableau n°4 – Pour la France entière, selon le 1er quantile, le 2ème quantile (la médiane) et le 3ème quantile :
-###### Paramètres météorologiques associés aux 14 657 lieux et dates de signalements comparés à ceux des mêmes dates
-###### mais pour un semis de lieux aléatoires (France, July 2017 – April 2020, soit 995 jours).
-###### Analyse des signalements et des données Météo DSK (non plus moyennées mais DSK brutes) pour la France entière
-###### Q1, Q2 (Médiane), Q3 et leurs IC
-###### Date : 25/08/2021
-###### Authors : Khaldoune Hilami, Vincent Godard
-##############################################################################################################################
-###### Code figure n°7 – Profils météorologiques associés aux 14 657 lieux et dates de signalements comparés à ceux des mêmes 
-###### dates, mais pour un semis de lieux aléatoires (France, January 17th 2017 – April 5th 2020, soit 995 jours).
-###### Code Tableau n°5 – En Île-de-France, selon le 1er décile, la moyenne et le 9ème décile :
-###### Paramètres météorologiques associés aux 14 657 lieux et dates de signalements comparés à ceux des mêmes dates
-###### mais pour un semis de lieux aléatoires (France, January 2017 – April 2020, soit 995 jours).
-###### Code Tableau n°6 – En Alsace-Lorraine, selon le 1er décile, la moyenne et le 9ème décile :
-###### Paramètres météorologiques associés aux 14 657 lieux et dates de signalements comparés à ceux des mêmes dates
-###### mais pour un semis de lieux aléatoires (France, January 2017 – April 2020, soit 995 jours).
-###### Code Tableau n°7 – En Rhône-Alpes, selon le 1er décile, la moyenne et le 9ème décile :
-###### Paramètres météorologiques associés aux 14 657 lieux et dates de signalements comparés à ceux des mêmes dates
-###### mais pour un semis de lieux aléatoires (France, January 2017 – April 2020, soit 995 jours).
-##############################################################################################################################
+####################################################################################################
+## Code pour Tableau n°4 – Pour la France entière, selon le 1er quantile, le 2ème quantile
+## (la médiane) et le 3ème quantile :
+## Paramètres météorologiques associés aux 14 657 lieux et dates de signalements comparés à ceux des 
+## mêmes datesmais pour un semis de lieux aléatoires (France, July 2017 – April 2020, soit 995 jours).
+## Analyse des signalements et des données Météo DSK (non plus moyennées mais DSK brutes) 
+## pour la France entière Q1, Q2 (Médiane), Q3 et leurs IC
+####################################################################################################
+## Date : 25/08/2021
+## Authors : Khaldoune Hilami, Vincent Godard
+####################################################################################################
+## Code figure n°7 – Profils météorologiques associés aux 14 657 lieux et dates de signalements
+## comparés à ceux des mêmes dates, mais pour un semis de
+##  lieux aléatoires (France, January 17th 2017 – April 5th 2020, soit 995 jours).
+## Code Tableau n°5 – En Île-de-France, selon le 1er décile, la moyenne et le 9ème décile :
+## Paramètres météorologiques associés aux 14 657 lieux et dates de signalements comparés à ceux des
+##  mêmes dates mais pour un semis de lieux aléatoires (France, January 2017 - April 2020, soit 995 jours).
+## Code Tableau n°6 – En Alsace-Lorraine, selon le 1er décile, la moyenne et le 9ème décile :
+## Paramètres météorologiques associés aux 14 657 lieux et dates de signalements comparés à ceux des 
+## mêmes datesmais pour un semis de lieux aléatoires (France, January 2017 - April 2020, soit 995 jours).
+## Code Tableau n°7 – En Rhône-Alpes, selon le 1er décile, la moyenne et le 9ème décile :
+## Paramètres météorologiques associés aux 14 657 lieux et dates de signalements comparés à ceux des 
+## mêmes datesmais pour un semis de lieux aléatoires (France, January 2017 – April 2020, soit 995 jours).
+####################################################################################################
 
-#_____________________________ Preparation de la donnee  _________________________________________#
+#_____________________________ Preparation de la donnee  __________________________________________#
 
 #### 1. Mise en place de l’environnement de travail
 setwd('./')
@@ -138,122 +141,123 @@ rm(list = c(paste('humdata_winter', 17:19, '_long', sep=''),
                         paste('DSKdata_winter', 17:19, '_long', sep=''),
                                 paste('DSKdata_winter', 17:19, '_short', sep='') ) )
 
-#______________________________________ Definition des fonctions  ___________________________________________#
+#______________________________________ Definition des fonctions  _________________________________#
 
 
 ### 5. Boostrap pour stabiliser indicateurs et intervalles de confiance du t.test d'une moyenne
 # Cf. médiane/quantile Poinsot, 2005, R pour les statophobes, pp.13-1510
 
-## Fonction de Calcul d'un IC en utilisant le Bootstrap pour des petits échantillons ou avec une absence
-# de normalité avérée (cf. section 3.4 de Poinsot, p.14)
+## Fonction de Calcul d'un IC en utilisant le Bootstrap pour des petits échantillons
+#  ou avec une absence de normalité avérée (cf. section 3.4 de Poinsot, p.14)
 ic_calculator <- function(param, calcul){
         
-        ## création d'une table 1000x3 index vides
-        ## pour initialiser les dimensions et la valeurs des cellules du dataframe qui suit
-        ## création d'un DF initialisé avec les valeurs de c1 pour les cellules et le nombre de lignes.
-        humDF <- data.frame("lower_quantile"=c(0:999),"middl_quantile"=c(0:999),"upper_quantile"=c(0:999))*NA 
+## création d'une table 1000x3 index vides
+## pour initialiser les dimensions et la valeurs des cellules du dataframe qui suit
+## création d'un DF initialisé avec les valeurs de c1 pour les cellules et le nombre de lignes.
+humDF <- data.frame("lower_quantile"=c(0:999),"middl_quantile"=c(0:999),"upper_quantile"=c(0:999))*NA 
 
-        ## boucle qui calcule 1000 fois sur un échantillon de 50 tirages les centiles 25, 50 et 75 à partir du vecteur index (cf. infra)
-        ## /!\ il faut exclure les NA du calcul de la moyenne également /!\
-        ## Calcule conditionnel selon l’option de la variable calcul
-        # Calcul de quartiles
-        if(calcul=='quartile'){
-                
-                for (i in 1:1000){
-                        humDF$lower_quantile[i] <- quantile(sample(param, 50, replace=T), 0.25, na.rm = T);
-                        humDF$middl_quantile[i] <- quantile(sample(param, 50, replace=T), 0.50, na.rm = T);
-                        humDF$upper_quantile[i] <- quantile(sample(param, 50, replace=T), 0.75, na.rm = T);
-                }
-        # Calcul de deciles        
-        }else if (calcul=='decile'){
-                
-                for (i in 1:1000){
-                        humDF$lower_quantile[i]    <- quantile(sample(param, 50, replace=T), 0.1, na.rm = T);
-                        humDF$middl_quantile[i]    <- mean(sample(param, 50, replace=T), na.rm = T);
-                        humDF$upper_quantile[i]    <- quantile(sample(param, 50, replace=T), 0.9, na.rm = T);
-                }
-                
-        } else{
-                print('Aucune opération valide demandée')
-        }
-
-        moy_quant1 <- mean(humDF$lower_quantile) ## moyenne des 1000 1er quantiles
-        moy_quant2 <- mean(humDF$middl_quantile) ## moyenne des 1000 2eme quantiles
-        moy_quant3 <- mean(humDF$upper_quantile) ## moyenne des 1000 3eme quantiles
-
-        humDF_C25.sort=sort(humDF$lower_quantile)
-        humDF_C50.sort=sort(humDF$middl_quantile)
-        humDF_C75.sort=sort(humDF$upper_quantile)
-
-        ## recuperation des bornes de l’IC
-        quant1_IC_bas <- humDF_C25.sort[25]
-        quant1_IC_haut <- humDF_C25.sort[975]
-
-        ## recuperation des bornes de l’IC
-        quant2_IC_bas <- humDF_C50.sort[25]
-        quant2_IC_haut <- humDF_C50.sort[975]
-
-        ## recuperation des bornes de l’IC
-        quant3_IC_bas <- humDF_C75.sort[25]
-        quant3_IC_haut <- humDF_C75.sort[975]
-
-        # La fonction retourne ce vecteur numeric contenant les 
-        # trois moyennes et leur intervalle de confiance
-        ic_vector = c(moy_quant1, quant1_IC_bas, quant1_IC_haut,
-                        moy_quant2, quant2_IC_bas, quant2_IC_haut,
-                          moy_quant3, quant3_IC_bas, quant3_IC_haut)
-        # arrondire a deux decimales
-        ic_vector <- round(ic_vector, digits=2)
+## boucle qui calcule 1000 fois sur un échantillon de 50 tirages les centiles
+## 25, 50 et 75 à partir du vecteur index (cf. infra)
+## /!\ il faut exclure les NA du calcul de la moyenne également /!\
+## Calcule conditionnel selon l’option de la variable calcul
+# Calcul de quartiles
+if(calcul=='quartile'){
         
-        return(ic_vector)
+  for (i in 1:1000){
+        humDF$lower_quantile[i] <- quantile(sample(param, 50, replace=T), 0.25, na.rm = T)
+        humDF$middl_quantile[i] <- quantile(sample(param, 50, replace=T), 0.50, na.rm = T)
+        humDF$upper_quantile[i] <- quantile(sample(param, 50, replace=T), 0.75, na.rm = T)
+  }
+# Calcul de deciles        
+}else if (calcul=='decile'){
+        
+  for (i in 1:1000){
+        humDF$lower_quantile[i] <- quantile(sample(param, 50, replace=T), 0.1, na.rm = T)
+        humDF$middl_quantile[i] <- mean(sample(param, 50, replace=T), na.rm = T)
+        humDF$upper_quantile[i] <- quantile(sample(param, 50, replace=T), 0.9, na.rm = T)
+  }
+        
+} else{
+        print('Aucune opération valide demandée')
+}
+
+moy_quant1 <- mean(humDF$lower_quantile) ## moyenne des 1000 1er quantiles
+moy_quant2 <- mean(humDF$middl_quantile) ## moyenne des 1000 2eme quantiles
+moy_quant3 <- mean(humDF$upper_quantile) ## moyenne des 1000 3eme quantiles
+
+humDF_C25.sort=sort(humDF$lower_quantile)
+humDF_C50.sort=sort(humDF$middl_quantile)
+humDF_C75.sort=sort(humDF$upper_quantile)
+
+## recuperation des bornes de l’IC
+quant1_IC_bas <- humDF_C25.sort[25]
+quant1_IC_haut <- humDF_C25.sort[975]
+
+## recuperation des bornes de l’IC
+quant2_IC_bas <- humDF_C50.sort[25]
+quant2_IC_haut <- humDF_C50.sort[975]
+
+## recuperation des bornes de l’IC
+quant3_IC_bas <- humDF_C75.sort[25]
+quant3_IC_haut <- humDF_C75.sort[975]
+
+# La fonction retourne ce vecteur numeric contenant les 
+# trois moyennes et leur intervalle de confiance
+ic_vector = c(moy_quant1, quant1_IC_bas, quant1_IC_haut,
+                moy_quant2, quant2_IC_bas, quant2_IC_haut,
+                  moy_quant3, quant3_IC_bas, quant3_IC_haut)
+# arrondire a deux decimales
+ic_vector <- round(ic_vector, digits=2)
+
+return(ic_vector)
 
 }
 
 ## Fonction de fabrication des tables statistiques d’IC
 ic_table_maker <- function(reportingdf, randomdf, paramvector, calcul){
                 
-        #liste vide a remplir
-        ic_table <- list()
-        
-        # boucle de calcule implementant la fonction quantile
-        # avec filtrage par le vecteur vectornames sur le dataframe humdata
-        for (name in paramvector ){
-        
-                # isolation de la colonne cible pour chaque iteration de la boucle
-                param_hum <- reportingdf[,name]
-                # affectation dans une variable intermediaire
-                result_hum <- ic_calculator(param_hum, calcul)
-                # isolation de la colonne cible pour chaque iteration de la boucle
-                param_dsk <- randomdf[,name]
-                # affectation dans une variable intermediaire
-                result_dsk <- ic_calculator(param_dsk, calcul)
-                # insertion du vecteru numeric resultant dans la liste
-                ic_table[[paste(name,'_reporting', sep='')]] <- result_hum
-                # insertion du vecteru numeric resultant dans la liste
-                ic_table[[paste(name,'_random', sep='')]] <- result_dsk
-                rm(list='param_hum','result_hum','param_dsk','result_dsk')
-        
-        }
-        
-        # transformation (cast) de la list en data.frame
-        ic_table <- as.data.frame(ic_table)
-        # transposition du tableau (data.frame)
-        ic_table <- as.data.frame(t(ic_table))
-        
-        # Creation d’un vecteur de nom de colonne pour le tableau transpose
-        nom_de_colonne = c('moy_quant1', 'quant1_IC_bas', 'quant1_IC_haut',
-                              'moy_quant2', 'quant2_IC_bas', 'quant2_IC_haut',
-                                'moy_quant3', 'quant3_IC_bas', 'quant3_IC_haut')
-        
-        # renommage des colonnes
-        colnames(ic_table) <- nom_de_colonne
-        
-        ## export au format csv (de-commenter en cas de besoin)
-        # date <- format(Sys.time(), "%A_%b_%d_%Hh%Mm%Ss_%Y")
-        # filename <- paste('ic', calcul, date, '.csv', sep = '_')
-        # write.csv(ic_table, filename )
-        
-        return(ic_table)
+  #liste vide a remplir
+  ic_table <- list()
+  
+  # boucle de calcule implementant la fonction quantile
+  # avec filtrage par le vecteur vectornames sur le dataframe humdata
+  for (name in paramvector ){
+  
+          # isolation de la colonne cible pour chaque iteration de la boucle
+          param_hum <- reportingdf[,name]
+          # affectation dans une variable intermediaire
+          result_hum <- ic_calculator(param_hum, calcul)
+          # isolation de la colonne cible pour chaque iteration de la boucle
+          param_dsk <- randomdf[,name]
+          # affectation dans une variable intermediaire
+          result_dsk <- ic_calculator(param_dsk, calcul)
+          # insertion du vecteru numeric resultant dans la liste
+          ic_table[[paste(name,'_reporting', sep='')]] <- result_hum
+          # insertion du vecteru numeric resultant dans la liste
+          ic_table[[paste(name,'_random', sep='')]] <- result_dsk
+          rm(list='param_hum','result_hum','param_dsk','result_dsk')
+  
+  }
+  
+  # transformation (cast) de la list en data.frame
+  ic_table <- as.data.frame(ic_table)
+  # transposition du tableau (data.frame)
+  ic_table <- as.data.frame(t(ic_table))
+  
+  # Creation d’un vecteur de nom de colonne pour le tableau transpose
+  nom_de_colonne = c('moy_quant1', 'quant1_IC_bas', 'quant1_IC_haut',
+                        'moy_quant2', 'quant2_IC_bas', 'quant2_IC_haut',
+                          'moy_quant3', 'quant3_IC_bas', 'quant3_IC_haut')
+  
+  # renommage des colonnes
+  colnames(ic_table) <- nom_de_colonne
+  
+  ## export au format csv (de-commenter en cas de besoin)
+  # date <- format(Sys.time(), "%A_%b_%d_%Hh%Mm%Ss_%Y")
+  # filename <- paste('ic', calcul, date, '.csv', sep = '_')
+  # write.csv(ic_table, filename )
+  
+  return(ic_table)
 
 }
 
@@ -422,9 +426,7 @@ kwcox_table <- function (dsk_paramnames, mf_paramnames, test='wilcox'){
   
 }
 
-
-
-#___________________________________________ Programme principal ________________________________________________________#
+#__________________________________________ Programme principal ___________________________________#
 
 ### Vecteur de caracteres contenant les parametres meteo a traiter
 vectornames <- c("temperature",  "temperaturelow", "temperaturehigh", "humidity",
